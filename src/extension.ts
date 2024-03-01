601023750177b7d9c3255076d98107be4e095d08
Lukada-taiya <lukmanadam43@gmail.com>
2024-03-01 21:29:32 +0000

    Worked on #7. Outlining general layout of project files

--- src/extension.ts
+++ src/extension.ts
@@ -9,13 +9,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let startCommand = vscode.commands.registerCommand('typeon.start', () => { 
-		start();
-	});
+	let startCommand = vscode.commands.registerCommand('typeon.start', start);
 
-	let stopCommand = vscode.commands.registerCommand('typeon.stop', () => { 
-		stop();
-	});
+	let stopCommand = vscode.commands.registerCommand('typeon.stop', stop);
 
 	context.subscriptions.push(startCommand);
 	context.subscriptions.push(stopCommand);
