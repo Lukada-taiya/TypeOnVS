84ab81c62db3055d298e746cbc7f6a031e8c294a
Lukada-taiya <lukmanadam43@gmail.com>
2024-03-12 22:25:21 +0000

    Added shortcuts for arrow keys

--- src/listener.ts
+++ src/listener.ts
@@ -1,32 +1,35 @@
-import * as vscode from 'vscode';
-const listener = () => {
-    let keyedText = ''; 
-    vscode.workspace.onDidChangeTextDocument((event) => {
-        // Check if the change was a single character insert
-        if (event.contentChanges.length === 1 && event.contentChanges[0].text.length === 1) {
-            // Get the inserted text
-            let insertedText = event.contentChanges[0].text; 
-            keyedText += insertedText;
-            if(keyedText.length >= 2) { 
-
-                if (checkKeySequence(keyedText, 'ei')) { 
-                }else if(checkKeySequence(keyedText, 'df')) {
-
-                }else if(checkKeySequence(keyedText, 'jk')) {
-
-                }else if(checkKeySequence(keyedText, 'dk')) {
-
-                } 
-                keyedText = '';
-            }
-        } 
-    });
-    
-    const checkKeySequence = (sequence: string, target: string): boolean => {        
-        const sortedSequence = sequence.split('').sort().join('');
-        const sortedTarget = target.split('').sort().join('');
-            
-        return sortedSequence === sortedTarget;
+import * as vscode from "vscode";
+import arrowKeysFunction from "./shortcuts/arrowKeys";
+const listener = () => {    
+  let keyedText = "";
+  vscode.workspace.onDidChangeTextDocument((event) => {
+    // Check if the change was a single character insert
+    if (
+      event.contentChanges.length === 1 &&
+      event.contentChanges[0].text.length === 1
+    ) {
+      // Get the inserted text
+      let insertedText = event.contentChanges[0].text;
+      keyedText += insertedText;
+      if (keyedText.length >= 2) {
+        if (checkKeySequence(keyedText, "ei")) { 
+            arrowKeysFunction('up');
+        } else if (checkKeySequence(keyedText, "df")) {
+            arrowKeysFunction('right');
+        } else if (checkKeySequence(keyedText, "jk")) {
+            arrowKeysFunction('left');
+        } else if (checkKeySequence(keyedText, "dk")) {
+            arrowKeysFunction('down');
+        }
+        keyedText = "";
+      }
     }
-}
+  });
+  const checkKeySequence = (sequence: string, target: string): boolean => {
+    const sortedSequence = sequence.split("").sort().join("");
+    const sortedTarget = target.split("").sort().join("");
+
+    return sortedSequence === sortedTarget;
+  };
+};
 export default listener;
